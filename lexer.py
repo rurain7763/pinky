d73cca9036374a5d53f930b2c0772cf7b055e0c9
rurain7763 <rurain7763@gmail.com>
2024-11-08 15:37:08 +0900

    lexer 클래스 일부 구현

---
+++ lexer.py
@@ -0,0 +1,36 @@
+from tokens import *
+
+class Lexer:
+    def __init__(self, source):
+        self.source = source
+        self.start = 0
+        self.curr = 0
+        self.line = 1
+        self.tokens = []
+        pass
+
+    def advance(self):
+        ch = self.source[self.curr]
+        self.curr = self.curr + 1
+        return ch
+    
+    def add_token(self, token_type):
+        self.tokens.append(Token(token_type, self.source[self.start:self.curr]))
+
+    def tokenize(self):
+        while self.curr < len(self.source):
+            self.start = self.curr
+            ch = self.advance()
+            if ch == '+': 
+                self.add_token(TOK_PLUS)
+            elif ch == '-': 
+                self.add_token(TOK_MINUS)
+            elif ch == '*':
+                self.add_token(TOK_STAR)
+            elif ch == '/':
+                self.add_token(TOK_SLASH)
+        
+        return self.tokens
+            
+        
+        
--- pinky.py
+++ pinky.py
@@ -1,14 +1,18 @@
 import sys
+from tokens import *
+from lexer import *
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         raise SystemExit('Usage: python3 pinky.py <filename>')
-    
+
     filename =sys.argv[1]
     print(filename)
 
     with open(filename) as file:
         source = file.read()
-        print(source)
+        print("Lexer:")
+        tokens = Lexer(source).tokenize()
+        for token in tokens:
+            print(token)
 
-        # TODO: Tokenizer
